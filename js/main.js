b33c30b515d1042315df4062d144e6767be84259
Thiago <tdelossantos768@gmail.com>
2025-11-21 01:56:05 -0300

    soy down y no le di a guardar

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 const btnStrength = document.getElementById("btnStrength");
 const btnVolume = document.getElementById("btnVolume");
+
+btnStrength.addEventListener("click", () => {
+    console.log("elegiste strength");
+});
+
+btnVolume.addEventListener("click", () =>{
+    console.log("elegisste volume")
+});
